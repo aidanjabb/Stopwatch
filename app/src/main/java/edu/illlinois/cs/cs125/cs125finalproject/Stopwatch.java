2b62bdf064d39ac2a6de6432cd62f264e26a5702
brianwangc <briancw3@illinois.edu>
2018-04-26 16:26:03 -0500

    timer implemented

--- app/src/main/java/edu/illlinois/cs/cs125/cs125finalproject/Stopwatch.java
+++ app/src/main/java/edu/illlinois/cs/cs125/cs125finalproject/Stopwatch.java
@@ -1,6 +1,8 @@
 package edu.illlinois.cs.cs125.cs125finalproject;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -8,11 +10,25 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Stopwatch extends AppCompatActivity {
 
-    /** Default logging tag for messages from the main activity. */
+    /**
+     * Default logging tag for messages from the main activity.
+     */
     private static final String TAG = "Stopwatch:Main";
+    long miliSecond;
+    long start;
+    long timeBuff;
+    long update = 0L;
+    int second;
+    int minutes;
+    int miliseconds;
+    Handler handler = new Handler();
+    Button Start_Stop;
+    Button Reset;
+    TextView showTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +37,26 @@ public class Stopwatch extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button Start_Stop = findViewById(R.id.Start_Stop);
-        Start_Stop.setOnClickListener(new View.OnClickListener() {
+        Start_Stop = findViewById(R.id.Start_Stop);
+        Reset = findViewById(R.id.Reset);
+        showTime = findViewById(R.id.textView);
+       Start_Stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "Start/Stop button clicked");
                 if (Start_Stop.getText() == "Start") {
                     Start_Stop.setText("Stop");
+                    start = SystemClock.uptimeMillis();
+                    handler.postDelayed(runnable, 0);
                 } else {
                     Start_Stop.setText("Start");
+                    timeBuff += miliSecond;
+                    handler.removeCallbacks(runnable);
                 }
-
             }
 
         });
 
-        final Button Reset = findViewById(R.id.Reset);
         Reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,6 +64,22 @@ public class Stopwatch extends AppCompatActivity {
             }
         });
     }
+
+    public Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            miliSecond = SystemClock.uptimeMillis() - start;
+            update = timeBuff + miliSecond;
+            second = (int) (update / 1000);
+            minutes = second / 60;
+            second = second % 60;
+            miliseconds = (int) (update % 1000);
+            showTime.setText("" + minutes + ":"
+                    + String.format("%02d", second) + ":"
+                    + String.format("%03d", miliseconds));
+            handler.postDelayed(this, 0);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
